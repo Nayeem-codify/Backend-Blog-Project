8f47bad36ca9d54704835c7fc54e1b6bc367260b
usernameNayeem-codify <useremailwebdevlopment2306@gmail.com>
2024-08-25 23:51:21 +0600

    Blog'

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Blog from './component/blog'
+
+const App = () => {
+  return (
+    <div className='py-10'>
+      <Blog/>
+    </div>
+  )
+}
+
+export default App

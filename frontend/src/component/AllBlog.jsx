f6fef7b07f35676430a3898df603736ac1f7e867
usernameNayeem-codify <useremailwebdevlopment2306@gmail.com>
2024-09-09 01:56:53 +0600

    list

--- frontend/src/component/AllBlog.jsx
+++ frontend/src/component/AllBlog.jsx
@@ -2,71 +2,39 @@ import React from "react";
 
 const AllBlog = () => {
   return (
-    <div className=" bg-red-300 h-(90vh) p-10">
-      <ul class="max-w-md divide-y divide-gray-200">
-        <li class="py-3 sm:py-4">
-          <div class="flex items-center space-x-4 rtl:space-x-reverse">
-            <div class="flex-shrink-0"></div>
-            <div class="flex-1 min-w-0">
-              <p class="text-sm font-medium">Nayeem Islam</p>
-              <p class="text-sm truncate">email@flowbite.com</p>
-            </div>
-            <div class="inline-flex items-center text-base font-semibold">
-              $4164
-            </div>
-          </div>
-        </li>
-      </ul>
-
-      <li class="py-3 sm:py-4">
-        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-          <div class="flex-shrink-0"></div>
+    <ul class="max-w-md divide-y divide-gray-200 bg-red-100 h-(80vh) p-10 overflow-y-scroll  ">
+      <li class="py-10">
+        <div class="flex  flex-col items-start">
           <div class="flex-1 min-w-0">
-            <p class="text-sm font-medium">Bonnie Green</p>
-            <p class="text-sm truncate">email@flowbite.com</p>
+            <p class="text-sm font-medium text-gray-900 truncate">Neil Sims</p>
+            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+              email@flowbite.com
+            </p>
           </div>
-          <div class="inline-flex items-center text-base font-semibold">
-            $3467
-          </div>
+          <blockquote class="text-xl italic font-semibold text-gray-900">
+            <p>
+              "Flowbite is just awesome. It contains tons of predesigned
+              components and pages starting from login screen to complex
+              dashboard. Perfect choice for your next SaaS application."
+            </p>
+          </blockquote>
+        </div>
+        <div className="flex mt-10">
+          <button
+            type="button"
+            class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+          >
+            Edit
+          </button>
+          <button
+            type="button"
+            class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 "
+          >
+            Delete
+          </button>
         </div>
       </li>
-      <li class="py-3 sm:py-4">
-        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-          <div class="flex-shrink-0"></div>
-          <div class="flex-1 min-w-0">
-            <p class="text-sm font-medium">Michael Gough</p>
-            <p class="text-sm truncate">email@flowbite.com</p>
-          </div>
-          <div class="inline-flex items-center text-base font-semibold">
-            $67
-          </div>
-        </div>
-      </li>
-      <li class="py-3 sm:py-4">
-        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-          <div class="flex-shrink-0"></div>
-          <div class="flex-1 min-w-0">
-            <p class="text-sm font-medium">Thomas Lean</p>
-            <p class="text-sm truncate">email@flowbite.com</p>
-          </div>
-          <div class="inline-flex items-center text-base font-semibold">
-            $2367
-          </div>
-        </div>
-      </li>
-      <li class="pt-3 pb-0 sm:pt-4">
-        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-          <div class="flex-shrink-0"></div>
-          <div class="flex-1 min-w-0">
-            <p class="text-sm font-medium ">Lana Byrd</p>
-            <p class="text-sm truncate ">email@flowbite.com</p>
-          </div>
-          <div class="inline-flex items-center text-base font-semibold">
-            $367
-          </div>
-        </div>
-      </li>
-    </div>
+    </ul>
   );
 };
 
